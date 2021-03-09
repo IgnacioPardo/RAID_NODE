5427576a74024c52d5d1eeca7709ac2d0446706e
Ignacio Pardo <ignacio.pardo@ort.edu.ar>
2021-03-09 15:05:31 +0000

    node self awareness and recognition,  get, set and usage db methods in current node

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, send_from_directory
-import codecs
+from flask import Flask, send_from_directory, request, Response
+import codecs, json
 import os
+
+#DB
+from database import DB
+
+defaults = {}
+db = DB("db.p", defaults)
 
 #Threading
 from threading import Thread
@@ -38,8 +44,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return 
+	return '<a href="'+node_loc+'">' + node_loc + '</a>'
+
+@app.route('/usage')
+def usage():
+	print(request.remote_addr)
+	return {"ram": db.ram_usage_p(), "disk": db.disk_usage_p()}
+
+@app.route('/getAll')
+@app.route('/get')
+@app.route('/get/<_id>')
+def _get(_id=None):
+	if _id:
+		return db.get(_id)
+	return db.getAll()
+
+@app.route('/set', methods=['POST'])
+@app.route('/set/<_id>', methods=['POST'])
+def _set(_id=None):
+	if request.method == 'POST':
+		try:
+			_id = db.set(json.loads(request.form["request"]), _id)
+			return {'id_set': _id}
+		except:
+			return 400
+	return 400
 
 if __name__ == '__main__':
 	#Run server forever
+	node_loc = str("https://" + os.getenv("REPL_SLUG") + "." + os.getenv("REPL_OWNER") + ".repl.co").lower()
+
 	keep_alive()
